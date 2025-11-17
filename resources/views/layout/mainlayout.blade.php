2dcb09e82dcec435c244b64e58c32d1d966065b2
Nicho <nicholasleroy83@gmail.com>
2025-11-17 14:59:22 +0700

    FIX: Remove Vite directive, use Bootstrap CDN only

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -10,8 +10,6 @@
     
     <!-- Bootstrap CSS dari CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    
-    @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 
 <body class="d-flex flex-column min-vh-100">
