33ccff5efcfa20835a9c6b3a933eeb0af4162d76
Nicho <nicholasleroy83@gmail.com>
2026-01-04 21:06:16 +0700

    fixing again

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,41 +9,22 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         $bakeryItems = [
-            'Croissant',
-            'Donut',
-            'Bagel',
-            'Muffin',
-            'Cupcake',
-            'Bread Roll',
-            'Danish',
-            'Eclair',
-            'Pretzel',
-            'Scone',
-            'Tart',
-            'Pie',
-            'Cake',
-            'Cookie',
-            'Brownie',
-            'Macaron',
-            'Baguette',
-            'Sourdough',
-            'Cinnamon Roll',
-            'Cheesecake',
-            'Tiramisu',
-            'Panettone',
-            'Strudel',
-            'Waffle'
+            'Croissant', 'Donut', 'Bagel', 'Muffin', 'Cupcake',
+            'Bread Roll', 'Danish', 'Eclair', 'Pretzel', 'Scone',
+            'Tart', 'Pie', 'Cake', 'Cookie', 'Brownie',
+            'Macaron', 'Baguette', 'Sourdough', 'Cinnamon Roll',
+            'Cheesecake', 'Tiramisu', 'Panettone', 'Strudel', 'Waffle'
         ];
 
-        $name = fake()->randomElement($bakeryItems);
+        $name = $this->faker->randomElement($bakeryItems);
 
         return [
             'name' => $name,
             'image' => "https://picsum.photos/seed/" . uniqid() . "/640/480",
-            'short_description' => fake()->sentence(10),
-            'long_description' => fake()->paragraphs(3, true),
-            'price' => fake()->numberBetween(15000, 50000),
-            'weight' => fake()->numberBetween(80, 500) ,
+            'short_description' => $this->faker->sentence(10),
+            'long_description' => $this->faker->paragraphs(3, true),
+            'price' => $this->faker->numberBetween(15000, 50000),
+            'weight' => $this->faker->numberBetween(80, 500),
         ];
     }
 }
